1481f5592b0081a356e23f453f8014b02246cab1
sumantmann <sumantmann@gmail.com>
2016-12-19 22:21:10 +0530

    raw setup done

--- indian_company_price/__init__.py
+++ indian_company_price/__init__.py
@@ -1,12 +1,26 @@
 import datetime
 import dateutil.parser
 import requests
-
+from bs4 import BeautifulSoup
 
 def indian_company_price(company_symbol, start_date, end_date):
-        start_date = dateutil.parser.parse(start_date).date().strftime('%Y-%m-%d')
-        end_date = dateutil.parser.parse(end_date).date().strftime('%Y-%m-%d')
-        url = 'https://www.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol=' + company_symbol + '&segmentLink=3&symbolCount=2&series=ALL&dateRange=+&fromDate=' + start_date + '&toDate=' + end_date + '&dataType=PRICEVOLUMEDELIVERABLE'
+        start_date = dateutil.parser.parse(start_date).date().strftime('%d-%m-%Y')
+        end_date = dateutil.parser.parse(end_date).date().strftime('%d-%m-%Y')
+        url = 'https://www.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol=' + company_symbol + '&segmentLink=3&symbolCount=2&series=ALL&dateRange=+&fromDate=' + start_date + '&toDate=' + end_date + '&dataType=PRICEVOLUMEDELIVERABLE' 
         r = requests.get(url)
         if r.status_code == 200:
-                pass
+                bsObj = BeautifulSoup(r.text, "html5lib")
+                bs_filter = bsObj.findAll('tr')[1:]
+                bs_rows = [x.findAll('td', {"class":["number", "date"]}) for x in bs_filter]
+                e = []
+                for x in bs_rows:
+                        j = []
+                        for y in x:
+                                j.append(y.get_text())
+                        e.append(j)
+                return e
+        else:
+                print r.status_code
+
+print(indian_company_price('TCS', '01-12-2016', '20-12-2016'))
+
--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
         name='indian_company_prices',
         versions='0.1',
-        scripts=['indian_company_price']
+        scripts=['indian_company_price'],
+        description='Function to get eod pricing data of all indian companies',
+        author='Sumant Mann',
+        author_email='sumantmann@yahoo.com',
+        url='https://github.com/sumantmann/indian_company_prices',
+        download_url='https://github.com/sumantmann/indian_company_prices/tarball/0.1',
+        keywords=['Finance', 'Indian Company Pricing'],
+        classifiers=['Development Status :: 3 - Alpha', 'Programming Language :: Python'],
+#        packages=find_packages(exclude=['bs4', 'html5lib']),
+        install_requires=['bs4', 'html5lib'],
+        entry_points={
+                'console_scripts': [
+                            'indian_company_prices=indian_company_prices:indian_company_prices',
+                                    ],
+                },
 )
